95276903ef076c890f3a234cc1d6ecf8f2f55075
VictorAlicino <victor.alicino@gmail.com>
2023-03-28 17:22:19 -0300

    Sei la

--- src/NumericRepresentation.h
+++ src/NumericRepresentation.h
@@ -18,9 +18,13 @@ class NumericRepresentation {
 private:
     //
     bool* binary;
+    std::string binary_string;
     uint8_t decimal;
     uint8_t octal;
     uint8_t hexadecimal;
+
+    uint8_t bit_size;
+    uint8_t* pins;
 
     std::string to_binary(uint8_t value);
     std::string to_decimal(uint8_t value);
@@ -29,8 +33,14 @@ private:
 public:
     NumericRepresentation(uint8_t bit_size, uint8_t* pins);
     void update(bool force_update);
+    std::string get_value(base type);
+
+    void read_bytes_from_ic();
 
 };
+
+bool* read_bytes_from_ic
+std::string numeric_representation_to_string(uint8_t* binary);
 
 
 #endif //_SD_LCD_TO_I2C_NUMERICREPRESENTATION_H
